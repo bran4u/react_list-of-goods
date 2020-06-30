5cd7966835a44263b41504ffc944b604871a9026
bran4u <s.oselskiy@gmail.com>
2020-07-01 00:21:28 +0300

    Updates

---
+++ precache-manifest.e5c119a5b37e92c78bdea315fb98f183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39794424199a7d223ec9fa5904743171",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0cf4aef2aa9b089f66fc",
+    "url": "./static/css/main.f0dd9aa7.chunk.css"
+  },
+  {
+    "revision": "0deea3e5989f4fd07403",
+    "url": "./static/js/2.bc235ad9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc235ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf4aef2aa9b089f66fc",
+    "url": "./static/js/main.61a48fc8.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
